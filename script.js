4c7b26ace80d747c12f1f5635ea6a604f54c9704
Mesut <mesutahmettravaci@gmail.com>
2024-02-15 23:52:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const darkModeToggle = document.getElementById('darkModeToggle');
-
-// Add event listener to the toggle button
-darkModeToggle.addEventListener('click', () => {
-    // Toggle dark mode class on the body
-    document.body.classList.toggle('dark-mode');
-    
-    // Toggle text on the button
-    if (document.body.classList.contains('dark-mode')) {
-        darkModeToggle.textContent = 'Light Mode';
+const bgToggle = document.getElementById('darkMode');
+var whitexts = document.querySelectorAll('.whitext');
+var blacktexts = document.querySelectorAll('.blacktext');
+var navigatorbackground = document.querySelectorAll('#navigator');
+darkMode.addEventListener('change', () => {
+    if (darkMode.checked) {
+        document.body.style.backgroundColor = 'white';
+        whitexts.forEach(function(i) {
+            i.style.color = 'black';
+        });
+        blacktexts.forEach(function(i) {
+            i.style.color = 'white';
+        });
+        navigatorbackground.forEach(function(i) {
+            i.style.backgroundColor = '#b7b7b7';
+        });
     } else {
-        darkModeToggle.textContent = 'Dark Mode';
+        document.body.style.backgroundColor = '#282828';
+        whitexts.forEach(function(i) {
+            i.style.color = 'white';
+        });
+        blacktexts.forEach(function(i) {
+            i.style.color = 'black';
+        });
+        navigatorbackground.forEach(function(i) {
+            i.style.backgroundColor = '#3a3a3a';
+        });
     }
 });
