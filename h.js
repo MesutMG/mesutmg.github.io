5e944c4b11043404485c4e1b4d7dcf7746355a81
Mesut <mesutahmettravaci@gmail.com>
2024-02-16 23:26:35 +0100

    Add files via upload

---
+++ h.js
@@ -0,0 +1,29 @@
+const bgToggle = document.getElementById('darkMode');
+var whitexts = document.querySelectorAll('.whitext');
+var blacktexts = document.querySelectorAll('.blacktext');
+var navigatorbackground = document.querySelectorAll('#navigator');
+darkMode.addEventListener('change', () => {
+    if (!darkMode.checked) {
+        document.body.style.backgroundColor = '#282828';
+        whitexts.forEach(function(i) {
+            i.style.color = 'white';
+        });
+        blacktexts.forEach(function(i) {
+            i.style.color = 'black';
+        });
+        navigatorbackground.forEach(function(i) {
+            i.style.backgroundColor = '#3a3a3a';
+        });
+    } else {
+        document.body.style.backgroundColor = 'white';
+        whitexts.forEach(function(i) {
+            i.style.color = 'black';
+        });
+        blacktexts.forEach(function(i) {
+            i.style.color = 'white';
+        });
+        navigatorbackground.forEach(function(i) {
+            i.style.backgroundColor = '#b7b7b7';
+        });
+    }
+});
